3187decb73b1a758bc854c8ebeffbbdc3ac6d911
Ondra Chaloupka <ochaloup@redhat.com>
2018-03-20 16:30:36 +0100

    adding some more info on properties printing

--- src/main/java/org/jboss/ochaloup/PropertiesServlet.java
+++ src/main/java/org/jboss/ochaloup/PropertiesServlet.java
@@ -2,6 +2,8 @@ package org.jboss.ochaloup;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map.Entry;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,16 +14,22 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name="PropertiesServlet", urlPatterns={"/properties"})
 public class PropertiesServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-        
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         out.println("EAP QE");
-        
+
+        out.print("<h1>System properties</h1>");
         for(String key : System.getProperties().stringPropertyNames()) {
           String value = System.getProperties().getProperty(key);
-          out.println(key + " => " + value);
+          out.printf("%s => %s<br/>", key, value );
+        }
+
+        out.print("<h1>Env properties</h1>");
+        for(Entry<String, String> entry : System.getenv().entrySet()) {
+            out.printf("%s => %s<br/>", entry.getKey(), entry.getValue());
         }
     }
 }
--- src/main/java/org/jboss/ochaloup/UserTransactionServlet.java
+++ src/main/java/org/jboss/ochaloup/UserTransactionServlet.java
@@ -15,20 +15,28 @@ import javax.transaction.UserTransaction;
 @WebServlet(name="UserTransactionServlet", urlPatterns={"/txn"})
 public class UserTransactionServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-        
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
-    	try {
-	    	InitialContext ic = new InitialContext();               
-	    	UserTransaction utx = (UserTransaction) ic.lookup("java:comp/UserTransaction");
-	    	out.printf("<p>User transaction found: %s</p>", utx);
-    	} catch (Throwable t) {
-    		out.println("<pre>");
-    		t.printStackTrace(out);
-    		out.println("</pre>");
-    	}
+        try {
+            InitialContext ic = new InitialContext();
+            UserTransaction utx = (UserTransaction) ic.lookup("java:comp/UserTransaction");
+            out.printf("<p>User transaction found at 'java:comp/UserTransaction': %s, status: %s</p>", utx, utx != null ? utx.getStatus() : "UNKNOWN");
+            utx.begin();
+            utx.commit();
+
+            UserTransaction utx2 = (UserTransaction) ic.lookup("java:jboss/UserTransaction");
+            out.printf("<p>User transaction found at 'java:jboss/UserTransaction': %s, status: %s</p>", utx2, utx != null ? utx.getStatus() : "UNKNOWN");
+            utx2.begin();
+            utx2.commit();
+
+        } catch (Throwable t) {
+            out.println("<pre>");
+            t.printStackTrace(out);
+            out.println("</pre>");
+        }
     }
 }
